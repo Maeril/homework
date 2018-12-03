00a239625508df07e7176152212484a5de43819e
Maeril <Maeril@Avesta.local>
2018-12-03 23:16:55 +0100

    avancee 03122018 soir

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,14 +6,20 @@
 /*   By: myener <myener@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 14:14:42 by myener            #+#    #+#             */
-/*   Updated: 2018/12/03 14:59:55 by myener           ###   ########.fr       */
+/*   Updated: 2018/12/03 22:48:05 by myener           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-#define BUFF_SIZE 32 /*taille d'exemple dans le sujet, a modif pr tester*/
+# define BUFF_SIZE 32 /*taille d'exemple dans le sujet, a modif pr tester*/
 
-    int     get_next_line(const int fd, char **line);
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+int     get_next_line(const int fd, char **line);
 
 #endif
