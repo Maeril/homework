e4be7e924b4abafbbf43a3d19dfd0892eaf80807
Maeril <Maeril@Avesta.local>
2019-02-02 17:10:14 +0100

    Debut ft_printf

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myener <myener@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/02 15:52:45 by myener            #+#    #+#             */
+/*   Updated: 2019/02/02 17:09:53 by myener           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef FT_PRINTF_H
+
+# define FT_PRINTF_H
+# include <stdarg.h>
+
+typedef struct	s_tetri
+{
+	char			**tetri;
+	char			key;
+	int				x;
+	int				y;
+	struct s_tetri	*next;
+}				t_tetri;
+
+int		ft_printf(const char *format);
+
+#endif
