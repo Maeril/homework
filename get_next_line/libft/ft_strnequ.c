09cfef03be6496795362bdb2657ce10323e445bf
Marie-shirine YENER <myener@e2r9p13.42.fr>
2019-02-04 16:25:30 +0100

    edits libft int > size_t

--- get_next_line/libft/ft_strnequ.c
+++ get_next_line/libft/ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: myener <myener@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 15:31:07 by myener            #+#    #+#             */
-/*   Updated: 2018/12/03 13:55:57 by myener           ###   ########.fr       */
+/*   Updated: 2018/11/27 16:11:39 by myener           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@ int		ft_strnequ(const char *s1, const char *s2, size_t n)
 {
 	if (!s1 || !s2)
 		return (0);
-	return (ft_strncmp(s1, s2, n) == 0) ? 1 : 0;
+	if (ft_strncmp(s1, s2, n) == 0)
+		return (1);
+	return (0);
 }
