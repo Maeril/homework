a1c313539909d840ec722f0f8181024492b045a2
Maeril <Maeril@Avesta.local>
2019-06-04 15:39:31 +0200

    avancee mai 2019

--- exam_training/practice/ft_itoa_base.c
+++ exam_training/practice/ft_itoa_base.c
@@ -6,10 +6,11 @@
 /*   By: myener <myener@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 17:33:51 by myener            #+#    #+#             */
-/*   Updated: 2019/05/27 17:59:33 by myener           ###   ########.fr       */
+/*   Updated: 2019/05/27 20:32:16 by myener           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <stdlib.h>
 
 char	*ft_itoa_base(int value, int base)
@@ -20,25 +21,32 @@ char	*ft_itoa_base(int value, int base)
 	int		neg;
 
 	neg = (value < 0 && base == 10) ? 1 : 0;
-	i = 0;
+	value = neg ? -value : value;
+	i = 1;
 	stock = value;
-	while ((value % base) >= 1)
-	{
-		value /= base;
+	while ((stock /= base) >= 1)
 		i++;
-	}
 	if (!(str = malloc(sizeof(char) * i + neg + 1)))
 		return (NULL);
-	value = stock;
+	str[i] = '\0';
+	i--;
+	while (i >= 0)
+	{
+		if (value % base < 10)
+			str[i + neg] = (value % base) + '0';
+		else
+			str[i + neg] = (value % base) + 'A' - 10;
+		value /= base;
+		i--;
+	}
 	if (neg)
 		str[0] = '-';
-	while (i > 0)
-	{
-		if (base <= 10)
-			str[i--] = (value % base) + '0';
-		else if (base > 10)
-			str[i--] = (value % base) + 'A' - 10;
-		value /= base;
-	}
 	return (str);
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc == 3)
+		printf("resultat = %s\n", ft_itoa_base(atoi(argv[1]), atoi(argv[2])));
+	return (0);
 }
