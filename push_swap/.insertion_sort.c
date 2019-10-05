0aa0b46d229b8110d1658b5e6547fed99afe4532
Marie-shirine YENER <myener@e3r9p13.42.fr>
2019-10-05 17:07:53 +0200

    05102019

--- push_swap/.insertion_sort.c
+++ push_swap/.insertion_sort.c
@@ -6,36 +6,30 @@
 /*   By: myener <myener@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/19 14:13:23 by myener            #+#    #+#             */
-/*   Updated: 2019/10/01 15:44:36 by myener           ###   ########.fr       */
+/*   Updated: 2019/10/05 17:00:08 by myener           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-struct node
+t_pslist	*insert_sort(t_pslist **head, int data)
 {
-  	int data;
-	struct node *next;
-};
+	t_pslist *temp;
+	t_pslist *prev;
+	t_pslist *ptr;
 
-struct node *head = NULL;
-
-void insert_sort(int data)
-{
-	struct node *temp = head;
-	struct node *prev = NULL;
-	struct node *ptr;
-
-	ptr = (struct node*)malloc(sizeof(struct node));
+	temp = *head;
+	ptr = malloc(sizeof(t_pslist));
 	ptr->data = data;
 	ptr->next = NULL;
+	prev = NULL;
 	if(temp == NULL) //Executes when linked list is empty
 	{
 		ptr->next = NULL;
 		head = ptr;
 		return ;
 	}
-	if(data<temp->data) //Executes if given data is less than data in first node of linked list
+	if(data < temp->data) //Executes if given data is less than data in first node of linked list
 	{
 		ptr->next = head;
 		head = ptr;
@@ -43,47 +37,21 @@ void insert_sort(int data)
 	}
 	else
 	{
-		while(temp!=NULL)
+		while(temp != NULL)
 		{
-			if(data>temp->data) //Traverse to location we want to insert the node + 1 node
+			if(data > temp->data) //Traverse to location we want to insert the node + 1 node
 			{
-				prev=temp;
-				temp=temp->next;
+				prev = temp;
+				temp = temp->next;
 				continue;
 			}
 			else //Insert the node
 			{
-				prev->next=ptr;
-				ptr->next=temp;
+				prev->next = ptr;
+				ptr->next = temp;
 				return;
 			}
 		}
 		prev->next=ptr; //Insert node at last
 	}
-}
-
-void print()
-{
-	struct node *temp=head;
-	printf("\nList:");
-	while(temp!=NULL)
-	{
-		printf("\n%d ",temp->data);
-		temp=temp->next;
-	}
-}
-
-int main()
-{
-	insert_sort(23);
-	print();
-	insert_sort(10);
-	print();
-	insert_sort(15);
-	print();
-	insert_sort(45);
-	print();
-	insert_sort(50);
-	print();
-	return 0;
 }
