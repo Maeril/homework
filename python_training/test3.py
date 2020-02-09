9368fd8e69db69d17fb8e0b9f98243b90c873661
Maeril <Maeril@Avesta.local>
2020-02-10 00:22:12 +0100

    10022020_0022_python

---
+++ python_training/test3.py
@@ -0,0 +1,25 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    test3.py                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: myener <myener@student.42.fr>              +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/02/09 23:49:48 by myener            #+#    #+#              #
+#    Updated: 2020/02/10 00:05:25 by myener           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import os, sys, string, regex
+
+def main():
+	pattern = string.join(sys.argv[1:])
+	filename = os.environ['HOME'] + '/.telbase'
+	prog = regex.compile(pattern, regex.casefold)
+	f = open(filename, 'r')
+	while 1:
+		line = f.readline()
+		if not line: break # End of file
+		if prog.search(line) >= 0:
+			print(string.strip(line))
+main()
