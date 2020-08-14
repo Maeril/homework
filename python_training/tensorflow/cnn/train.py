f06cca65f7137274016544551dc476a356d139ca
Maeril <Maeril@Avesta.local>
2020-08-14 11:08:38 +0200

    14082020_1108

--- python_training/tensorflow/cnn/train.py
+++ python_training/tensorflow/cnn/train.py
@@ -6,23 +6,23 @@
 #    By: myener <myener@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/08/01 01:13:34 by myener            #+#    #+#              #
-#    Updated: 2020/08/02 02:42:53 by myener           ###   ########.fr        #
+#    Updated: 2020/08/14 11:08:35 by myener           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-# set the matplotlib backend so figures can be saved in the background
+# Set the matplotlib backend so figures can be saved in the background
 import matplotlib
 matplotlib.use("Agg")
 
-# import the necessary packages
+# Import the necessary packages
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.optimizers import Adam
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from smallervggnet.smallervggnet import SmallerVGGNet
-import matplotlib.pyplot as plt
 from imutils import paths
+import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 import random
@@ -30,7 +30,7 @@ import pickle
 import cv2
 import os
 
-# construct the argument parse and parse the arguments
+# Construct the argument parsing
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required=True,
 	help="path to input dataset (i.e., directory of images)")
@@ -42,7 +42,7 @@ ap.add_argument("-p", "--plot", type=str, default="plot.png",
 	help="path to output accuracy/loss plot")
 args = vars(ap.parse_args())
 
-# initialize the number of epochs to train for, initial learning rate,
+# Initialize the number of epochs to train for, initial learning rate,
 # batch size, and image dimensions
 EPOCHS = 100
 INIT_LR = 1e-3
@@ -57,44 +57,44 @@ imagePaths = sorted(list(paths.list_images(args["dataset"])))
 random.seed(42)
 random.shuffle(imagePaths)
 
-# loop over the input images
+# Loop over the input images
 for imagePath in imagePaths:
-	# load the image, pre-process it, and store it in the data list
+	# Load the image, pre-process it, and store it in the data list
 	image = cv2.imread(imagePath)
 	image = cv2.resize(image, (IMAGE_DIMS[1], IMAGE_DIMS[0]))
 	image = img_to_array(image)
 	data.append(image)
-	# extract the class label from the image path and update the
-	# labels list
+	# Extract the class label from the image path and update the
+	# Labels list
 	label = imagePath.split(os.path.sep)[-2]
 	labels.append(label)
 
-# scale the raw pixel intensities to the range [0, 1]
+# Scale the raw pixel intensities to the range [0, 1]
 data = np.array(data, dtype="float") / 255.0
 labels = np.array(labels)
 print("[INFO] data matrix: {:.2f}MB".format(
 	data.nbytes / (1024 * 1000.0)))
-# binarize the labels
+# Binarize the labels
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
-# partition the data into training and testing splits using 80% of
+# Partition the data into training and testing splits using 80% of
 # the data for training and the remaining 20% for testing
 (trainX, testX, trainY, testY) = train_test_split(data,
 	labels, test_size=0.2, random_state=42)
 
-# construct the image generator for data augmentation
+# Construct the image generator for data augmentation
 aug = ImageDataGenerator(rotation_range=25, width_shift_range=0.1,
 	height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
 	horizontal_flip=True, fill_mode="nearest")
 
-# initialize the model
+# Initialize the model
 print("[INFO] compiling model...")
 model = SmallerVGGNet.build(width=IMAGE_DIMS[1], height=IMAGE_DIMS[0],
 	depth=IMAGE_DIMS[2], classes=len(lb.classes_))
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="categorical_crossentropy", optimizer=opt,
 	metrics=["accuracy"])
-# train the network
+# Train the network
 print("[INFO] training network...")
 H = model.fit(
 	x=aug.flow(trainX, trainY, batch_size=BS),
@@ -102,7 +102,7 @@ H = model.fit(
 	steps_per_epoch=len(trainX) // BS,
 	epochs=EPOCHS, verbose=1)
 
-# save the model to disk
+# Save the model
 print("[INFO] serializing network...")
 model.save(args["model"], save_format="h5")
 # save the label binarizer to disk
@@ -111,7 +111,7 @@ f = open(args["labelbin"], "wb")
 f.write(pickle.dumps(lb))
 f.close()
 
-# plot the training loss and accuracy
+# Create a plot capturing the training loss and accuracy
 plt.style.use("ggplot")
 plt.figure()
 N = EPOCHS
